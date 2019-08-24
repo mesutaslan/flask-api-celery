2e7666ed40b9fba7c4fd3d4645e635e641d72c3b
Mesut Aslan <mesaslan@gmail.com>
2019-08-24 22:46:23 +0100

    Add migrate commmand

---
+++ migrate.py
@@ -0,0 +1,15 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from myapp.contacts.models import db
+from myapp.settings import DevConfig
+from app import create_app
+
+app = create_app(DevConfig)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
